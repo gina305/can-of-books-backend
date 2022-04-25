07575396003609811d07bbc94e82fce611e976f1
Gina Napier <napier.gina@gmail.com>
2022-04-25 18:36:53 -0500

    Updated files

--- seed.js
+++ seed.js
@@ -2,24 +2,34 @@
 
 require('dotenv').config();
 const mongoose = require('mongoose');
+const Book = require('./model/books.js');
 mongoose.connect(process.env.DB_URL);
-const Book = require('./model/books.js')
+
 
 async function seed() {
-
+//Create a book based on schema
   await Book.create({
     title: "Aries Book",
     description: "Book about aries",
     status: "read",
-
   });
   console.log('Aries was added');
+
+  //Create a book based on schema
   await Book.create({
     title: "Jennas Book",
     description: "Book about Jenna",
-    status: "read",
+    status: "unread",
   });
   console.log('Aries was added');
+
+  //Create a book based on schema
+  await Book.create({
+    title: "Chicken Book",
+    description: "Book about chickens",
+    status: "read",
+  });
+  console.log('Chicken book was added');
 
   //Disconnect from database
   mongoose.disconnect(process.env.DB_URL);
